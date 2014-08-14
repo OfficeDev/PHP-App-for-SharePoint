b5ffadf54ca24e9c17db0aadba9713711524dff8
Ricardo Loo <Ricalo@users.noreply.github.com>
2014-08-14 12:11:08 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    <title>Index</title>
+</head>
+<body>
+    ##Index Page
+</body>
+</html>
