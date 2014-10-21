cce9f299a8cd3906daa311f88f672e808ca668dd
Ricardo Loo <Ricalo@users.noreply.github.com>
2014-10-20 18:55:10 -0700

    Updating to switch branches... app is not working and need to go to other branch that was working yesterday

--- PHP/index.php
+++ PHP/index.php
@@ -5,6 +5,7 @@
 $loader = require 'vendor/autoload.php';
 
 // Include the configuration class
+require_once 'TokenHelper.php';
 require_once 'config.php';
 
 // The PHP web application should be started with a SharePoint context, 
@@ -86,64 +87,66 @@ else {
     	echo $refreshToken;
     	$grant = new \League\OAuth2\Client\Grant\RefreshToken();
     	$accessToken = $provider->getAccessToken($grant, ['refresh_token' => $refreshToken, 'resource' => $resource]);
+    	//$tokenHelper = new TokenHelper($_POST['SPSiteUrl'], $_POST['SPAppToken']);
+    	//$accessToken = $tokenHelper->GetAccessToken();
     	echo '<p>';
-    	echo $accessToken;
+    	echo $accessToken->accessToken;
     	
     	//This is the REST endpoint that we are sending our request to
     	$apiUrl = $_POST['SPSiteUrl'] . '/_api/web/title';
     	$client = new Guzzle\Service\Client();
     	$request = $client->createRequest('GET', $apiUrl, [
-    			'headers' => ['Authorization' => 'Bearer ' + $accessToken->toString(),
+    			'headers' => ['Authorization' => 'Bearer ' . $accessToken->accessToken,
     							'Accept' => 'application/json;odata=verbose'
     			]
     	]);
     	
     	$response = $client->send($request);
     	echo $response->getBody(true);
-    	
+    	echo 'something';
     }
     catch(Exception $exception){
         echo '<p>An exception occurred getting an access token: ' . 
              $exception->getMessage().'</p>';
         exit;
     }
-
-    //Initialize a CURL instance
+//     //Initialize a CURL instance
 //     $curlObj = curl_init();
 //     // //This is the REST endpoint that we are sending our request to
 //     $apiUrl = $_POST['SPSiteUrl'] . '/_api/web/title';
 //     curl_setopt($curlObj, CURLOPT_URL, $apiUrl);
 //     // Indicate that we want the response back as a string
 //     curl_setopt($curlObj, CURLOPT_RETURNTRANSFER, 1);
-//     // FIXME: By default, HTTPS does not work with curl. 
+//     // FIXME: By default, HTTPS does not work with curl.
 //     //   This is a workaround for developer environments.
 //     // Using this CURL option exposes the PHP server to man-in-the-middle attacks.
 //     //   Remove in production scenarios.
 //     curl_setopt($curlObj, CURLOPT_SSL_VERIFYPEER, false);
 //     //Add accept and authorization headers, along with the access token
 //     curl_setopt($curlObj, CURLOPT_HTTPHEADER, array('Authorization: Bearer '.
-//                                                     $accessToken->access_token, 
-//                                                     'Accept:application/json;odata=verbose'));
-
-//     // Execute the request and get the response in the 
+//     		$accessToken->access_token,
+//     		'Accept:application/json;odata=verbose'));
+     
+//     // Execute the request and get the response in the
 //     //   output variable as a string
 //     $output = curl_exec($curlObj);
 //     // Close curl instance to free up system resources
 //     curl_close($curlObj);
 //     // Decode the response using the JSON decoder
 //     $jsonResult = json_decode($output);
-    
+     
 //     if($jsonResult->d != null){
-//         // Print the result to the page
-//         echo '<p>Querying the REST endpoint '. $apiUrl . '<br/>';
-//         echo 'Result: ' . $jsonResult->d->Title.'</p>';
+//     	// Print the result to the page
+//     	echo '<p>Querying the REST endpoint '. $apiUrl . '<br/>';
+//     	echo 'Result: ' . $jsonResult->d->Title.'</p>';
 //     }
 //     else{
-//         // There was a problem with the request
-//         echo '<p>There was a problem querying the REST endpoint '. $apiUrl . '<br/>';
-//         echo 'Error code: ' . $jsonResult->error->code . '<br/>';
-//         echo 'Error message: '. $jsonResult->error->message->value . '</p>';
+//     	// There was a problem with the request
+//     	echo '<p>There was a problem querying the REST endpoint '. $apiUrl . '<br/>';
+//     	echo 'Error code: ' . $jsonResult->error->code . '<br/>';
+//     	echo 'Error message: '. $jsonResult->error->message->value . '</p>';
 //     }
+    
  }
  
  //*********************************************************
